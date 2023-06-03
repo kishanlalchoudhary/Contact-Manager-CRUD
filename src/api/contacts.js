f0c0af057b872de5b2ed67a0ee4ddedf7f3103d2
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-06-04 01:22:37 +0530

    fixed db.json bug

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:3006/",
   baseURL:
-    "https://my-json-server.typicode.com/kishanlalchoudhary/Contact-Manager-React-Axios-CRUD/db",
+    "https://my-json-server.typicode.com/kishanlalchoudhary/Contact-Manager-React-Axios-CRUD/",
 });
