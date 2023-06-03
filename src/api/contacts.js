1f4a6c05b494c3ea75ff0bdcc5bdf603c687ee22
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-06-04 00:47:18 +0530

    added db.json and updated base url in axios

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3006/",
+  // baseURL: "http://localhost:3006/",
+  baseURL:
+    "https://my-json-server.typicode.com/kishanlalchoudhary/Contact-Manager-React-Axios-CRUD/contacts/",
 });
